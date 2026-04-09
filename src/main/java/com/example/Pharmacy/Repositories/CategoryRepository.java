3895fb4c0317b35f9e0d48e0d8f23e2f1331b5ce
wickyhien18 <giaphien1008@gmail.com>
2026-04-09 23:24:27 +0700

    add service Category

--- src/main/java/com/example/Pharmacy/Repositories/CategoryRepository.java
+++ src/main/java/com/example/Pharmacy/Repositories/CategoryRepository.java
@@ -2,6 +2,11 @@ package com.example.Pharmacy.Repositories;
 
 import com.example.Pharmacy.Entities.Categories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Categories, Long> {
+
+    @Query(value = "SELECT * FROM categories WHERE category_id = :id", nativeQuery = true)
+    Categories findByIdDetail(@Param("id") Integer id);
 }
