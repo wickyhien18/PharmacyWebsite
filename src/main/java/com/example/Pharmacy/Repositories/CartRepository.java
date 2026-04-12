0d50804602c9b35d7bda14ee2dac10cf35eb1571
wickyhien18 <giaphien1008@gmail.com>
2026-04-12 20:49:56 +0700

    add getAll

--- src/main/java/com/example/Pharmacy/Repositories/CartRepository.java
+++ src/main/java/com/example/Pharmacy/Repositories/CartRepository.java
@@ -5,7 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Carts,Long> {
     @Query(value = "SELECT * FROM carts WHERE cart_id = :id", nativeQuery = true)
     Carts findByIdDetail(@Param("id") Integer id);
+
+    @Query(value = "select * from carts " +
+            "join cart_item on carts.cart_id = cart_item.cart_id " +
+            "join users on carts.user_id = users.user_id",nativeQuery = true)
+    List<Carts> getAll();
 }
--- src/main/java/com/example/Pharmacy/Repositories/OrderRepository.java
+++ src/main/java/com/example/Pharmacy/Repositories/OrderRepository.java
@@ -5,7 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Orders,Long> {
     @Query(value = "SELECT * FROM orders WHERE order_id = :id", nativeQuery = true)
     Orders findByIdDetail(@Param("id") Integer id);
+
+    @Query(value = "select * from orders " +
+            "join order_item on orders.order_id = order_item.order_id " +
+            "join users on orders.user_id = users.user_id", nativeQuery = true)
+    List<Orders> getAll();
 }
