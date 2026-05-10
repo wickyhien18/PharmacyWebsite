88bf91f8505eae1263c4ec9e7dfcd898aa83fcd7
wickyhien18 <giaphien1008@gmail.com>
2026-05-10 15:50:12 +0700

    add health

--- src/main/java/Pharmacy/Config/SecurityConfig.java
+++ src/main/java/Pharmacy/Config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 //1 Request - 1 Token
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/health").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/medicines/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/manufacturers/**").permitAll()
---
+++ src/main/java/Pharmacy/Controllers/HealthController.java
@@ -0,0 +1,17 @@
+package Pharmacy.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
